2bf1d3bad2c520d48030329a799827a67a4c2cb4
havakv <haavard.kvamme@gmail..com>
2019-01-14 13:20:23 +0100

    Working on deephit method

--- pycox/models/deephit.py
+++ pycox/models/deephit.py
@@ -1,9 +1,33 @@
 """Impleentation of the deephit model, but currently for single event cause
 (not compteeting risk).
 """
+import torch
 from pyth import Model, tuplefy
 
-def pmf_nll(phi, y, d, reduction='elementwise_mean', _epsilon=1e-7):
+def nll_pmf(phi, y, d, reduction='elementwise_mean', _epsilon=1e-7):
+    """Negative log-likelihood for survival data from probability mass function (pmf).
+    
+    Arguments:
+        phi {torch.tensor} -- Predictions. phi.softmax() gives the pmf estimate.
+        y {torch.tensor} -- Labels (0/1), with 1 at the event/censoring time.
+        d {torch.tensor} -- Indicator 1: event, 0: censoring.
+    
+    Keyword Arguments:
+        reduction {str} -- Reductions (default: {'elementwise_mean'})
+        _epsilon {float} -- For numerical stability. (default: {1e-7})
+    
+    Returns:
+        torch.tensor -- The nll.
+    """
+    # This is a more numerical stable version of:
+    # 
+    # d.view(-1)
+    # f = phi.softmax(1)
+    # s = 1. - f.cumsum(1)
+    # part1 = f.mul(y).sum(1).log().mul(d)
+    # part2 = s.mul(y).sum(1).log().mul(1. - d)
+    # loss =  - part1.add(part2)
+    # return loss.mean()
     d.view(-1)
     gamma = phi.max(1)[0]
     cumsum = phi.sub(gamma.view(-1, 1)).exp().cumsum(1)
@@ -17,4 +41,36 @@ def pmf_nll(phi, y, d, reduction='elementwise_mean', _epsilon=1e-7):
         return loss
     elif reduction == 'elementwise_mean':
         return loss.mean()
+    return loss.sum()
+
+
+def loss_deephit_l2(phi, y, d, sigma=1., reduction='elementwise_mean', _epsilon=1e-7):
+    idx_sort = y.nonzero()[:, 1].sort()[1]
+    phi, y, d = phi[idx_sort], y[idx_sort], d[idx_sort]
+    cif = phi.softmax(1).cumsum(1)
+    # f_si = phi.mul(y).sum(1)
+    loss = torch.zeros_like(d)
+    _acc = torch.zeros_like(d)
+    _etas = torch.zeros_like(d)
+    for row, idx in y[:-1].nonzero():
+#         acceptable = y[(row+1):, idx].eq(0.).float().mul(d[(row+1):]).mul(d[row])
+        acceptable = y[(row+1):, idx].mul(d[(row+1):]).eq(0.).float().mul(d[row])
+        if acceptable.sum() == 0:
+            loss[row] = 0.
+            continue
+        eta = (-cif[row, idx].sub(cif[(row+1):, idx]).div(sigma)).exp()
+#         print(eta)
+        loss[row] = eta.mul(acceptable).sum() / acceptable.sum()
+        _acc[row] = acceptable.sum()
+        _etas[row] = eta.mul(acceptable).sum()
+#     print('end')
+    if reduction == 'none':
+        return loss#, _acc, _etas
+    elif reduction == 'elementwise_mean':
+#         return loss.mean()
+        n = loss.nonzero().shape[0]
+        loss = loss.sum()
+        if n != 0:
+            loss = loss / n
+        return loss
     return loss.sum()
