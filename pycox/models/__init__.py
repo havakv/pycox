f945165526fa09c36889e1aa6c1c9e60b506fe7b
havakv <haavard.kvamme@gmail..com>
2019-05-07 12:59:21 +0200

    Removing old model implementations

--- pycox/models/__init__.py
+++ pycox/models/__init__.py
@@ -1,11 +1,3 @@
-# from .cox import CoxPH, CoxTime, CoxLifelines, CoxBase, CoxPHFunc, CoxTimeFunc
-# from .cox import CoxPH, CoxTime, CoxLifelines, CoxBase, CoxPHFunc, CoxTimeFunc
-# from .cox_pyth import CoxCC, CoxTime
-# from .high_level import CoxPHLinear, CoxPHReluNet, CoxTimeReluNet
-from . import cox, cox_cc
-from .cox import CoxPH
-from .cox_cc import CoxCC, CoxTime
-# from .torch_fitter import FitNet
 
-# legacy
-# CoxPH = CoxCC
+from pycox.models.cox import CoxPH
+from pycox.models.cox_cc import CoxCC, CoxTime
