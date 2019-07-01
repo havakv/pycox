de4311c5875bb88f37b44513c0e8d16aa71981b6
havakv <haavard.kvamme@gmail..com>
2019-07-01 15:10:36 +0200

    Testing

--- pycox/__init__.py
+++ pycox/__init__.py
@@ -6,8 +6,8 @@ __author__ = """Haavard Kvamme"""
 __email__ = 'haavard.kvamme@gmail.com'
 __version__ = '0.0.1'
 
-import datasets
-import evaluation
-import models
-import preprocessing
-import simulation
+import pycox.datasets
+import pycox.evaluation
+import pycox.models
+import pycox.preprocessing
+import pycox.simulation
