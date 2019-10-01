25dc35cecde9759bef40458025b903f0c46c5c94
Haavard Kvamme <haavard.kvamme@gmail.com>
2019-10-01 11:08:58 +0200

    Feature DeepHit for competing risks (#4)
    * Added competing risks verison of deephit
    
    * Added predict_cif
    
    * Updated examples and docs

--- pycox/models/__init__.py
+++ pycox/models/__init__.py
@@ -1,4 +1,4 @@
 
 from pycox.models.cox.cox import CoxPH
 from pycox.models.cox.cox_cc import CoxCC, CoxTime
-from pycox.models.deephit import DeepHitSingle
+from pycox.models.deephit import DeepHitSingle, DeepHit
