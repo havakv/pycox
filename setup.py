4bafbe0f43424114441f42c2d815a398bd233740
haavard <haavard.kvamme@gmail.com>
2024-09-02 13:34:08 +0200

    Update python version

--- setup.py
+++ setup.py
@@ -51,9 +51,6 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.6',
     ],
-    python_requires='>=3.6'
+    python_requires='>=3.8'
 )
